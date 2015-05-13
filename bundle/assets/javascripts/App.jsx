638917f52e88f0ba66faf697d110ce7ddab58ed4
brianswisher <bswisher@gmail.com>
2015-05-13 09:36:01 -0700

    initial WIP

---
+++ bundle/assets/javascripts/App.jsx
@@ -0,0 +1,5 @@
+class App extends require('react').Component {
+  render() {
+    return <div>App</div>;
+  }
+}
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,33 @@
 (function() {
   'use strict';
 
+  require("babel/register")({
+    // Optional ignore regex - if any filenames **do** match this regex then they
+    // aren't compiled
+    ignore: /node_modules/,
+
+    // Optional only regex - if any filenames **don't** match this regex then they
+    // aren't compiled
+    // only: /my_es6_folder/,
+
+    // See options above for usage
+    whitelist: [],
+    blacklist: [],
+
+    // Setting this will remove the currently hooked extensions of .es6, `.es`, `.jsx`
+    // and .js so you'll have to add them back if you want them to be used again.
+    extensions: [".es6", ".es", ".jsx", ".js"]
+  });
+
   var express = require('express');
   var router = express.Router();
+  // var App = React.createFactory(
+  //   require('../../bundle/assets/javascripts/App.jsx')
+  // );
 
   // catch all at bottom
   router.get('*', function(req, res) {
-    res.send('deployable');
+    res.send('deployable-isomorphic');
   });
 
   module.exports = router;
