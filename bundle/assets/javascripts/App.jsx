9fed7f5121bbaba6a28fb78c0bc5b48f13fcb7b2
brianswisher <bswisher@gmail.com>
2015-05-13 11:21:17 -0700

    full page markup w/ nested react component

--- bundle/assets/javascripts/App.jsx
+++ bundle/assets/javascripts/App.jsx
@@ -1,9 +1,43 @@
 import React from 'react';
+import HelloWorld from './app/HelloWorld.jsx';
 
-class HelloWorld extends React.Component {
+class App extends React.Component {
   render() {
-    return <p>Hello, world!</p>;
+    return (
+      <html>
+        <head lang="en">
+            <meta charSet="UTF-8"/>
+            <title>React App</title>
+        </head>
+        <body>
+          <div id="main">
+            <HelloWorld/>
+          </div>
+        </body>
+        <script type="text/javascript"
+                src={this._getModernizerPath()}></script>
+       </html>
+    );
+  }
+
+
+  _getAppPath() {
+    var filename = 'app.bundle.js';
+
+    if (this.props.ENV === 'development') {
+      filename =  '__' + filename;
+    }
+
+    return '/' + filename;
+  }
+
+  _getMessage() {
+    return 'hello world';
+  }
+
+  _getModernizerPath() {
+    return 'https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js';
   }
 }
 
-export default HelloWorld;
+export default App;
